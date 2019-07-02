65817b49e6a62babb43d008749e71fc590bc9130
jshin83 <jen.kyongah.lee@gmail.com>
2019-07-02 09:50:48 -0700

    initial commit

---
+++ src/main/java/com/js401/taskmaster/TaskmasterApplication.java
@@ -0,0 +1,13 @@
+package com.js401.taskmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmasterApplication.class, args);
+	}
+
+}
